1ae2bdeb07b68efb81499ccccbc568813eb402c7
nrd141 <nrd141@gmail.com>
2024-01-29 09:15:37 +0700

    add default route

--- src/server.js
+++ src/server.js
@@ -12,6 +12,13 @@ const init = async () => {
       }
     }
   })
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Welcome to my Notes API, to properly use it please refer to the documentation on https://github.com/nrd141/Notes-API/blob/main/README.md'
+    }
+  })
   server.route(routes)
   await server.start()
   console.log(`Server berjalan pada ${server.info.uri}`)
