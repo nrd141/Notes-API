7b5635ae1a73c2e26e733d30983305468fbbfcbd
nrd141 <nrd141@gmail.com>
2024-01-29 08:53:11 +0700

    fix update

--- src/handler.js
+++ src/handler.js
@@ -6,8 +6,9 @@ const addNoteHandler = (request, h) => {
 
   const id = nanoid(16)
   const createdAt = new Date().toISOString()
+  const updatedAt = createdAt
   const newNote = {
-    title, tags, body, id, createdAt
+    title, tags, body, id, createdAt, updatedAt
   }
   notes.push(newNote)
   const isSuccess = notes.filter((note) => note.id === id).length > 0
